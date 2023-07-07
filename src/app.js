1ab37fab0d65848aa38ec1a76d5aabb83a16f0f1
CLAUDIA HERNANDEZ <claudiahergre@gmail.com>
2023-07-07 14:01:05 +0200

    saludo chachi

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const app = express();
 //necesitamos esto para que puedan entrar objetos de tipo json
 app.use(express.json())
 
+app.get('/', (rec, res) => {
+    res.send('holigüis')
+})
 app.use('/api', require('./routes/api'))
 
 module.exports = app;
